b0c915f6298072ccd8d0055861aff073d5fc7557
rafacovez <adanescollante@gmail.com>
2023-02-02 16:06:51 -0400

    coded calculateResellPrice function

--- vehicle-purchase.js
+++ vehicle-purchase.js
@@ -35,5 +35,11 @@ export function chooseVehicle(option1, option2) {
  * @returns {number} expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Please implement the calculateResellPrice function');
+  if (age < 3) {
+    return originalPrice * 0.8;
+  } else if (age > 10) {
+    return originalPrice * 0.5;
+  } else {
+    return originalPrice * 0.7;
+  }
 }
